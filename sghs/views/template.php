1148555e48ac7acc758f24e5b8d0c23576e51b6a
LionardoGomiz <leo_071998@hotmail.com>
2017-08-23 13:02:41 -0500

    Add files via upload

---
+++ sghs/views/template.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Departamento de Edición e Investigación</title>
+
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="icon" href="views/images/sena.png">
+
+	<link rel="stylesheet" href="views/css/bootstrap.min.css">
+	<link rel="stylesheet" href="views/css/font-awesome.min.css">
+	<link rel="stylesheet" href="views/css/style.min.css">
+	<link rel="stylesheet" href="views/css/fonts.min.css">
+	<link rel="stylesheet" href="views/css/cssFancybox/jquery.fancybox.css">
+	<link rel="stylesheet" href="views/css/sweetalert.css">
+
+	<script src="views/js/jquery-2.2.0.min.js"></script>
+	<script src="views/js/bootstrap.min.js"></script>
+	<script src="views/js/jquery.fancybox.js"></script>
+	<script src="views/js/animatescroll.js"></script>
+	<script src="views/js/jquery.scrollUp.js"></script>
+	<script src="views/js/sweetalert.min.js"></script>
+
+</head>
+
+<body>
+
+
+
+	<div class="container-fluid">
+
+		<!--=====================================
+		CABEZOTE
+		======================================-->
+		
+		<?php include "modules/cabezote.php"; ?>
+		
+		<!--====  Fin de CABEZOTE  ====-->
+
+		<!--=====================================
+		SLIDE
+		======================================-->
+		
+		<?php include "modules/slide.php"; ?>
+
+		<!--====  Fin de SLIDE  ====-->
+
+		
+
+
+		<!--=====================================
+		ARTÍCULOS
+		======================================-->
+
+		<?php include "modules/articulos.php"; ?>
+
+		<!--====  Fin de ARTÍCULOS  ====-->
+
+		<!--=====================================
+			ARTÍCULO MODAL         
+		======================================-->
+
+		<?php include "modules/articuloModal.php"; ?>	
+
+    	<!--====  Fin de ARTICULO MODAL ====-->
+		
+	</div>
+
+
+
+
+<script src="views/js/script.min.js"></script>
+
+</body>
+</html>
+
+
